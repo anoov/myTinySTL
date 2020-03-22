b606d81d12f0042d90caf68c061d80dbcf648887
anoov <1374206329@qq.com>
2020-03-22 22:06:22 +0800

    增加list的构造和list的insert方法

--- myTinySTL/myList.h
+++ myTinySTL/myList.h
@@ -87,6 +87,49 @@ public:
     reference front() {return *begin();}
     reference back() {return *(-end());}
 
+protected:
+    //配置一个节点并传回
+    link_type get_node() {
+        return new list_node;
+    }
+    //释放一个节点
+    void put_node(link_type p) {delete p;}
+
+    //产生一个带有元素值的节点
+    link_type create_node(const T& x) {
+        auto p = get_node();
+        std::allocator<T>().construct(&p->data, x);
+        return p;
+    }
+    //析构并释放一个节点
+    void destroy(link_type p) {
+        std::allocator<T>().destroy(&p->data);
+        put_node(p);
+    }
+
+    //产生一个空链表
+    void empty_initialize() {
+        node = get_node();
+        node->next = node;
+        node->prev = node;
+    }
+
+public:
+    //构造函数
+    myList(){empty_initialize();}   //产生一个空链表
+
+    iterator insert(iterator position, const T& x) {
+        link_type p = create_node(x);
+        p->next = position.node;
+        p->prev = position.node->prev;
+        position.node->prev->next = p;
+        position.node->prev = p;
+        return p;
+    }
+
+    void push_back(const T& x) {
+        insert(end(), x);
+    }
 
 protected:
 };
